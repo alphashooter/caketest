0f7739c23d1ae1b2f8023e990d27aa92c829ef37
Andrey Strelkov <a.strelkov@plamee.com>
2015-07-16 12:12:20 +0600

    'utils' module extended

--- src/utils/dictutils.py
+++ src/utils/dictutils.py
@@ -1,3 +1,28 @@
+class sdict(dict):
+    def __init__(self, iterable=None, **kwargs):
+        if not iterable:
+            iterable = dict()
+        dict.__init__(self, iterable, **kwargs)
+        for key in self.keys():
+            if isinstance(self[key], dict):
+                self[key] = sdict(self[key])
+
+    def copy(self):
+        return sdict(self)
+
+    def has_key(self, k):
+        return self.__contains__(k)
+
+    def __contains__(self, item):
+        return dict.__contains__(self, str(item))
+
+    def __getitem__(self, item):
+        return dict.__getitem__(self, str(item))
+
+    def __setitem__(self, item, value):
+        return dict.__setitem__(self, str(item), value)
+
+
 def merge_objects(obj1, obj2):
     for key in obj2.keys():
         if key in obj1 and isinstance(obj1[key], dict) and isinstance(obj2[key], dict):
