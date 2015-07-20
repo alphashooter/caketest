f1289f5d6634b8a673ec7ad8b0e297bd23c6e8ff
Andrey Strelkov <a.strelkov@plamee.com>
2015-07-20 21:55:07 +0600

    Test utils added.

--- src/plamee/cakestory/test/__init__.py
+++ src/plamee/cakestory/test/__init__.py
@@ -1,10 +1,27 @@
+from importlib import import_module
+
+from plamee.Test import Test
 from plamee.cakestory import *
 
-Net.connect("m3highload-master.test.nsk.plamee.com")
-
-import fuel_request
-import bonus_score
-
 def run() :
-    fuel_request.run()
-    bonus_score.run()
+    Net.connect("m3highload-master.test.nsk.plamee.com")
+
+    import sys
+    from os import listdir
+    from os.path import isfile, dirname, join
+
+    dir = dirname(__file__)
+
+    files = listdir(dir)
+    files = filter(lambda file: isfile(join(dir, file)), files)
+    files = filter(lambda file: re.match(r"^.+\.py$", file) is not None, files)
+    files = map(lambda file: re.match(r"^(.+)\.py$", file).group(1), files)
+    files = filter(lambda file: re.match(r"^__.+__$", file) is None, files)
+
+
+    Test.start_test(files)
+
+    sys.path.extend([dir])
+    map(lambda file: import_module(file), files)
+
+    Test.finish_test()
