ec08af887e3aa3cec50219dd949b2e8ab3b65202
Andrey Strelkov <a.strelkov@plamee.com>
2015-07-31 15:50:41 +0600

    fuel_station test update.

--- src/plamee/cakestory/test/map/fuel_station.py
+++ src/plamee/cakestory/test/map/fuel_station.py
@@ -1,5 +1,20 @@
 from plamee.cakestory import *
 
 client = Client()
-if not client.map.last_chapter.force_finish():
-    raise RuntimeError("Test failed.")
+chapter = client.map.current_chapter
+
+while chapter is not None:
+    if chapter.id > 5:
+        if chapter.locks <= 3:
+            raise RuntimeError("Expected more than 3 locks for chapter %d." % chapter.id)
+
+    if chapter.is_locked:
+        chapter.force_buy_unlocks()
+        if chapter.unlocks != chapter.locks:
+            raise RuntimeError("Unlocks assertion failed for chapter %d." % chapter.id)
+        chapter.unlock()
+
+    if not chapter.force_finish():
+        raise RuntimeError("Cannot finish chapter %d" % chapter.id)
+
+    chapter = chapter.next
